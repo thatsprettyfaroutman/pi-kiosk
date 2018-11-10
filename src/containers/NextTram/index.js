f41d954efc5edafe5a92a0050f4266c1ab02d37b
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-11-11 00:19:50 +0200

    Added a message if there is enough time to take out the trash

--- src/containers/NextTram/index.js
+++ src/containers/NextTram/index.js
@@ -11,7 +11,14 @@ const NextTram = ({ trams }) => {
 
   const minutes = trams[0].arrival || 0
   let content = minutes
+
+  // There is time to take out the trash
+  if (minutes < 3) content = 'trash'
+
+  // Good time to leave
   if (minutes < 2) content = 'now'
+
+  // Gotta run!
   if (minutes < 1) content = 'hurry'
 
   return (
