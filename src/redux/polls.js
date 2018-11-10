0e7a915441497edf263e27f2ce30a92212863930
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-11-11 01:24:49 +0200

    Fixed bug with polling. Added try catch to polling

--- src/redux/polls.js
+++ src/redux/polls.js
@@ -24,17 +24,23 @@ export const startPolling = (() => {
     if (polling) return
     polling = true
 
-    const arrivals = await Promise.all(TRAM_STOPS.map( x => getTrams(x) ))
-    const sortedArrivals = arrivals
-      .reduce( (res, stopTimes) => [...res, ...stopTimes], [] )
-      .sort( (a, b) => a.arrival - b.arrival )
-      .filter( arrival => TRAM_LINES.includes(arrival.lineName) )
-    store.dispatch(setTrams(sortedArrivals))
+    try {
+      const arrivals = await Promise.all(TRAM_STOPS.map( x => getTrams(x) ))
+      const sortedArrivals = arrivals
+        .reduce( (res, stopTimes) => [...res, ...stopTimes], [] )
+        .sort( (a, b) => a.arrival - b.arrival )
+        .filter( arrival => TRAM_LINES.includes(arrival.lineName) )
+      store.dispatch(setTrams(sortedArrivals))
 
-    const weather = await getWeather()
-    store.dispatch(setWeather(weather))
+      const weather = await getWeather()
+      store.dispatch(setWeather(weather))
+
+    } catch (err) {
+      console.warn(err)
+    }
 
     await timeout(30000)
+    polling = false
     startPolling(store)
   }
 })()
