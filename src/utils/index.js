36d2b704f014558b2c554bd1f2a6cfd5e8f35b77
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-11-11 00:00:21 +0200

    Changed promises to async await

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
 import differenceInMinutes from 'date-fns/difference_in_minutes'
-
 import { WALK_MINUTES } from '../config'
 
 const WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q=Helsinki,FI&appid=7d05f32d3baaae6039efc0453838c0b1&units=metric'
@@ -13,124 +12,118 @@ export const getNowInSeconds = () => Math.round(Date.now() / 1000)
 
 
 
-export const getJson = url => new Promise(resolve => {
-  return fetch(url)
-    .then(res => {
-      switch (res.status) {
-        case 200:
-          const data = res.json()
-          return data
-
-        default:
-          resolve(null)
-      }
-    })
-    .then(data => {
-      resolve(data)
-    })
-    .catch(() => resolve({}))
-})
+export const getJson = async url => {
+  const res = await fetch(url)
+  if (res.status !== 200) throw new Error(res.status)
+  return await res.json()
+}
 
 
 
 
-export const getHslData = query => new Promise(resolve => {
-  fetch(TRAMS_URL, {
+export const getHslData = async query => {
+  const res = await fetch(TRAMS_URL, {
     method: 'post',
     headers: {
       'Content-Type': 'application/graphql'
     },
-    body: query
+    body: query,
   })
-    .then(res => {
-      if (res.status !== 200) throw new Error(res.status)
-      const data = res.json()
-      return data
-    })
-    .then(data => {
-      resolve(data)
-    })
-    .catch(err => {
-      resolve([])
-      console.warn(err)
-    })
-})
+
+  if (res.status !== 200) throw new Error(res.status)
+  return await res.json()
+}
 
 
 
 
-export const getTrams = ( stopId, startTime = getNowInSeconds()) => {
-  return new Promise(resolve => {
-    if (!stopId) return resolve([])
-    getHslData(`
-      {
-        stop(id:"${stopId}"){
-          name
-          gtfsId
-          stoptimesWithoutPatterns(
-            startTime:"${startTime}",
-            timeRange: 18000,
-            numberOfDepartures:4
-          ) {
-            scheduledArrival
-            scheduledDeparture
-            realtimeArrival
-            serviceDay
-            stopHeadsign
-            trip {
-              route {
-                gtfsId
-                longName
-                shortName
-              }
+export const getTrams = async ( stopId, startTime = getNowInSeconds() ) => {
+  const res = await getHslData(`
+    {
+      stop(id:"${ stopId }"){
+        name
+        gtfsId
+        stoptimesWithoutPatterns(
+          startTime:"${ startTime }",
+          timeRange: 18000,
+          numberOfDepartures:4
+        ) {
+          scheduledArrival
+          scheduledDeparture
+          realtimeArrival
+          serviceDay
+          stopHeadsign
+          trip {
+            route {
+              gtfsId
+              longName
+              shortName
             }
           }
         }
-      }`
-    )
-      .then(res => res.data.stop.stoptimesWithoutPatterns)
-      .then(res => {
-        const now = new Date()
-        resolve(res
-          .map(x => {
-            const arrivalDate = new Date((x.serviceDay + x.realtimeArrival) * 1000)
-            const arrivalInMinutes = differenceInMinutes(arrivalDate, now)
-            const arrival = arrivalInMinutes - WALK_MINUTES
-            return ({
-              arrival,
-              lineName: x.trip.route.shortName,
-            })
-          })
-          .filter(x => x.arrival >= 0)
-        )
-      })
-  })
-
-
-}
-
-
-
-
-
-export const getWeather = () => getJson(WEATHER_URL)
-  .then(res => {
-    if (!res || !res.main || !res.main.temp) return 0
-    return Math.round(res.main.temp)
-  })
-
-
-
-
-export const pollRefresh = ( interval = 10000 ) => {
-  setTimeout(() => {
-    fetch(window.location).then(res => {
-      if (res.status === 200) {
-        window.location = window.location
-        return
       }
+    }`
+  )
 
-      pollRefresh()
+  const stopTimes =
+    res &&
+    res.data &&
+    res.data.stop &&
+    res.data.stop.stoptimesWithoutPatterns
+
+  if ( !stopTimes ) return {}
+
+  const now = Date.now()
+
+  return stopTimes
+    .map(stopTime => {
+      const arrivalDate =
+        new Date((stopTime.serviceDay + stopTime.realtimeArrival) * 1000)
+      const arrivalInMinutes = differenceInMinutes(arrivalDate, now)
+      const arrival = arrivalInMinutes - WALK_MINUTES
+      return ({
+        arrival,
+        lineName: stopTime.trip.route.shortName,
+      })
     })
-  }, interval)
+    .filter( stopTime => stopTime.arrival >= 0 )
 }
+
+
+
+
+export const getWeather = async () => {
+  const res = await getJson(WEATHER_URL)
+  const temp =
+    res &&
+    res.main &&
+    res.main.temp
+
+  if ( !temp ) return 0
+
+  return Math.round(temp)
+}
+
+
+
+
+export const pollRefresh = async ( interval = 10000 ) => {
+  await timeout(interval)
+
+  for (let i = 0; i < 2; i++) {
+    let res = await fetch(window.location)
+    if ( res.status === 200 ) {
+      window.location = window.location
+      return
+    }
+    await timeout(i === 0 ? 1000 : 0)
+  }
+
+  pollRefresh(interval)
+}
+
+
+
+
+export const timeout = ms =>
+  new Promise(resolve => setTimeout(() => resolve(true), ms))
